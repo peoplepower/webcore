e130867844cbf623858afaa1cdc7df54b4c64f79
maxcoffer <maxcoffer@gmail.com>
2026-02-17 14:47:07 +0100

    Fix rollup options for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,24 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json' with {type: 'json'};
+
+const tsConfig = {
+  tsconfig: './tsconfig.bundle.json',
+  checkJs: false,
+};
+
+type BuildFormat = 'es' | 'cjs' | 'umd';
+
+// Get the file name for a given build format.
+const fileFormat = (format: BuildFormat): string => {
+  const ext = {
+    'es': 'module', // ES module (for bundlers)
+    'cjs': 'main', // CommonJS (for Node)
+    'umd': 'browser', // Browser-friendly UMD
+  };
+  return `${pkg[ext[format] as keyof typeof pkg]}`.replace('dist/', '');
+}
 
 export default defineConfig({
   esbuild: {
@@ -13,14 +32,15 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/webcore.ts'),
       formats: ['es', 'cjs'],
-      fileName: (format) => format === 'es' ? 'webcore.esm.js' : 'webcore.cjs.js',
+      fileName: (format) => fileFormat(format as BuildFormat),
     },
     minify: false,
     sourcemap: true,
-    target: 'es2020',
-    outDir: 'dist',
     rollupOptions: {
       external: ['axios', 'qs'],
+      plugins: [
+        typescript(tsConfig),
+      ],
     },
   },
   test: {
